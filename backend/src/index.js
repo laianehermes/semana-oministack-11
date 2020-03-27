4d187074cdd85394629b58824a5db22907e401b2
laianehermes <laiane.sp@hotmail.com>
2020-03-27 17:56:45 -0300

    Frondend e algumas alterações no back

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,10 @@
-const express = require('express');
-const cors = require(cors);
-const routes = require('./routes')
-const app = express();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-/**
- * Tipos de parametros:
- * 
- * Query Params: Parametros nomeados enviados na rota após "?" (Filtros, paginação)
- * Route Params: Parametros utilizados para identificar recursos
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
